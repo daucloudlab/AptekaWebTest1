f02799a27e81302330296b8d43806742396ea7f7
Daulet Baimbetov <daucloudlab@gmail.com>
2015-06-23 12:29:38 +0600

    (version 04)

--- medikament/urls.py
+++ medikament/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
-from medikament.views import all_medikaments, add_medikaments, add_medikaments_process
+from medikament.views import all_medikaments, add_medikaments
 
 urlpatterns = [
     url(r'^$', all_medikaments),
     url(r'^add/$', add_medikaments),
-    url(r'^add_process/$', add_medikaments_process),
+
 ]
