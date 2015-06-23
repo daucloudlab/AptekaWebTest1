7bab9e781a0a3ab49d99fcb4e6d49117e06e3b16
Daulet Baimbetov <daucloudlab@gmail.com>
2015-06-23 17:24:04 +0600

    (version 07) медикаменттер жойылады

--- medikament/urls.py
+++ medikament/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from medikament.views import all_medikaments, add_medikaments
+from medikament.views import all_medikaments, add_medikaments, del_medikaments
 
 urlpatterns = [
     url(r'^$', all_medikaments),
     url(r'^add/$', add_medikaments),
+    url(r'^del/$', del_medikaments),
 
 ]
