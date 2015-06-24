e6be2a94f3af3ad42d50498864bc0401aa8ce118
Daulet Baimbetov <daucloudlab@gmail.com>
2015-06-24 16:40:46 +0600

    (version 11) Медикаменттің мәліметтерін жартылай жаңарта алады

--- medikament/urls.py
+++ medikament/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
-from medikament.views import all_medikaments, add_medikaments, del_medikaments
+from medikament.views import all_medikaments, add_medikaments, del_medikaments,update_medikaments
 
 urlpatterns = [
     url(r'^$', all_medikaments),
     url(r'^add/$', add_medikaments),
     url(r'^del/$', del_medikaments),
+    url(r'^update/(?P<id>\w+)/$', update_medikaments)
 
 ]
